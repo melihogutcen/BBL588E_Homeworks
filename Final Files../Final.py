edcbdd344d7e70150b4f6f1bebbe089a66bc984f
Melih Yılmaz ÖGÜTCEN <43522440+melihogutcen@users.noreply.github.com>
2020-07-03 23:31:00 +0300

    Final Exam

--- Final Files../Final.py
+++ Final Files../Final.py
@@ -63,7 +63,6 @@ gray = 0.2989 * image[:,:,0] + 0.5870 * image[:,:,1] + 0.1140 * image[:,:,2]
 hist, bins_center = exposure.histogram(gray)
 plt.imshow(gray < val, cmap='gray', interpolation='nearest')
 plt.axis('off')
-
 #%%
 
 
